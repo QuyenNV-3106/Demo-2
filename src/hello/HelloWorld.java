477cad2bdeabb6e4f1736593ba8f5774464f338e
QuyenNV-3106 <band3106@gmail.com>
2021-08-31 22:46:39 +0700

    Second commit

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -12,5 +12,7 @@ package hello;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        
+        System.out.println("a + b = "+ (2+3));
     }
 }
